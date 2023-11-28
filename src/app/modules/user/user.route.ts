89a6aec650cf6f5e8587dcb610fb08fd048ba60f
zannatreya <zannatulbintabahar@yahoo.com>
2023-11-28 19:54:56 +0600

    create user api implemented

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,5 +2,6 @@ import express from 'express';
 import { userController } from './user.controller';
 const router = express.Router();
 router.post('/create-users', userController.createUser);
+router.get('/', userController.);
 
 export const UserRoutes = router;
